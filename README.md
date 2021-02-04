9f0bfdf9a7970ffad94e9909e5f3f538ff9b3e96
aaronsaldanha <74081133+aaronsaldanha@users.noreply.github.com>
2021-02-04 14:51:33 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# First-Game
+ Based on freeCodeCamp tutorial
---
+++ main.py
@@ -0,0 +1,58 @@
+print('Welcome to my first game!')
+
+name = input('What is your name? ')
+age = int(input('What is your age? '))
+print('Hello', name, 'you are', age, 'years old.')
+
+health = 10
+
+if age >= 18:
+    print('You are old enough to play!')
+
+    wants_to_play = input('Do you want to play? ').lower()
+    if wants_to_play == 'yes':
+        print('You are starting with', health, 'health.')
+        print("Let's play!")
+
+        left_or_right = input('First choice... Left or Right (left / right)? ')
+        if left_or_right == 'left':
+            ans = input(
+                'Nice, you follow the path and reach a lake... Do you swin across or go around (across / around)? '
+            )
+
+            if ans == 'around':
+                print(
+                    'You went around and reached the other side of tha lake.')
+            elif ans == 'across':
+                print(
+                    'You managed to get across, but were bit by a fish and lost 5 health.'
+                )
+                health -= 5
+            else:
+                print('Bye')
+
+            ans = input(
+                'You notice a house and a river. Which do you go (house/river)? '
+            )
+            if ans == 'house':
+                print(
+                    'You go to the house and are greted by the owner... He does not like you and you lose 5 health.'
+                )
+                health -= 5
+
+                if health <= 0:
+                    print('You now have 0 health and you lost the game...')
+                else:
+                    print('You have survived... You win!')
+
+            else:
+                print('You fell in the river and lost...')
+
+        else:
+            print('You fell down and lost...')
+
+    else:
+        print('Cya...')
+
+else:
+    print('You are not old enough to play...')
